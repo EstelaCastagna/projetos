bb006e7b2a52396a6afa5ae7afa97faf9d42241b
EstelaCastagna <estelacastagnadds@gmail.com>
2024-03-08 10:53:53 -0300

    2 commit

--- app/public/index.php
+++ app/public/index.php
@@ -1,52 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
-
-    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-</head>
-<body>
-    
-<!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-<!-- carousel -->
+<?php  
+require 'header.phtml';
+?>
 
 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
@@ -56,21 +10,21 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/img/carnes.png" class="d-block w-100" alt="...">
+      <img src="img/aperitivo.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="/img/molhos.png" class="d-block w-100" alt="...">
+      <img src="img/carnes.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="/img/aperitivo.png" class="d-block w-100" alt="...">
+      <img src="img/molhos.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
@@ -87,33 +41,36 @@
   </button>
 </div>
 
-<!-- contents -->
-
 <div class="container">
   <div class="row">
     <div class="col-sm">
-        <h1>Últimas notícias</h1>
-      Roteiro do Shrek foi clonado para um echo em php na aula de DDS no Senai Vitória. Entenda o caso.
+      <h2>Receita de Bolo</h2>
+      Bata as claras em neve e reserve.
+      Misture as gemas, a margarina e o açúcar até obter uma massa homogênea.
+      Acrescente o leite e a farinha de trigo aos poucos, sem parar de bater.
+      Por último, adicione as claras em neve e o fermento.
+      Despeje a massa em uma forma grande de furo central untada e enfarinhada.
+      Asse em forno médio 180 °C, preaquecido, por aproximadamente 40 minutos ou ao furar o bolo com um garfo, este saia limpo.
     </div>
     <div class="col-sm">
-        <h1>Suspensos e Humilhados</h1>
-        Alunos da turma de  DDS do Senai Vitória foram suspensos após trocarem teclas do teclado tentando escrever algo com penis. Após o período de afastamento, tiveram que apresentar um seminário sobre infrações para turma de PcD's.
-      
+    <h2>Receita de Pipoca</h2>
+    Misturar todos os ingredientes numa panela de fundo grosso;
+    Tampar e levar ao fogo médio, mexendo sempre a panela (use luvas de proteção térmica) até estourar todo o milho;
+    Assim que parar os estouros, tire imediatamente a pipoca da panela para não queimar e coloque na travessa em que vai servir.
     </div>
     <div class="col-sm">
-      One of three columns
+    <h2>Receita de Sopa</h2>
+    Em uma panela de pressão, refogue os cubos de carne;
+    Acrescente a cebola e refogue até ficar translúcida;
+    Coloque a páprica, o caldo de carne e a água;
+    Tampe a panela e, assim que pegar pressão, conte 20 minutos;
+    Tire a pressão e coloque os legumes, mantendo a tampa aberta. Cozinhe por 5 minutos;
+    Adicione o macarrão, cozinhe por 3 minutos;
+    Acerte o sal e salpique cheiro-verde.
     </div>
   </div>
 </div>
 
-<!-- footer -->
-
-
-
-<!-- <script src="/bootstrap/js/bootstrap.min.js"></script> -->
-<link href="/css/app.css">
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
-</body>
-</html>
+<?php  
+require 'footer.phtml';
+?>
